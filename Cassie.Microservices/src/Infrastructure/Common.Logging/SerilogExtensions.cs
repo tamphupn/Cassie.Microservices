a21099c5a1150f6e8a3b060295aac95d9ba791ab
tamphu.pn <tamphu.pn>
2023-07-10 14:10:02 +0700

    chore: update contracts and log service

---
+++ Cassie.Microservices/src/Infrastructure/Common.Logging/SerilogExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Common.Logging
+{
+    public static class SerilogExtensions
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure => (context, configuration) =>
+        {
+
+        }
+    }
+}
