720b2904a47b62ffe4fd793b31ff3c4afe69a806
phupham <tamphu.pn@gmail.com>
2023-07-21 10:12:26 +0700

    chore: update setting for docker-compose file

--- Cassie.Microservices/src/Infrastructure/Common.Logging/SerilogExtensions.cs
+++ Cassie.Microservices/src/Infrastructure/Common.Logging/SerilogExtensions.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.Extensions.Hosting;
-using Serilog;
+﻿//using Microsoft.Extensions.Hosting;
+//using Serilog;
 
-namespace Common.Logging
-{
-    public static class SerilogExtensions
-    {
-        public static Action<HostBuilderContext, LoggerConfiguration> Configure => (context, configuration) =>
-        {
-
-        }
-    }
-}
+//namespace Common.Logging
+//{
+//    public static class SerilogExtensions
+//    {
+//        public static Action<HostBuilderContext, LoggerConfiguration> Configure => (context, configuration) =>
+//        {
+            
+//        }
+//    }
+//}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Cassie.Microservices
+
+1. Using docker-compose
+- docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d --remove-orphans
