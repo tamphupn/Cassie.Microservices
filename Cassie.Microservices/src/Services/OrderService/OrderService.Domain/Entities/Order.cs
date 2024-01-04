f92c3b047d13911734c7058703ee4c6ed62860e4
phupham <tamphu.pn@gmail.com>
2024-01-05 02:35:31 +0700

    chore: update some file in the code

---
+++ Cassie.Microservices/src/Services/OrderService/OrderService.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Cassie.Contracts.Domains;
+
+namespace OrderService.Domain.Entities
+{
+	public class Order: EntityAuditBase<long>
+	{
+		public string UserName { get; set; }
+		public decimal TotalPrice { get; set; }
+		public string LastName { get; set; }
+		public string EmailAddress { get; set; }
+	}
+}
+
